50cb7699846276b65ac990d488df048ca7cd7c99
kelta-king <kushangshah131@gmail.com>
2024-03-30 09:58:29 +0530

    Theme game initiated

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,18 +3,44 @@ import './App.css';
 import themeDark from './themeDark';
 import themeLight from './themeLight';
 import { Box, Button } from '@mui/material';
+import { useState } from 'react';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
 
 function App() {
+	const [theme, setTheme] = useState("Light");
+
+	const handleThemeChange = (event) => {
+		if (event.target.checked) {
+			setTheme("Dark");
+		}
+		else {
+			setTheme("Light");
+		}
+	}
+
 	return (
 		<div className="App">
-			<ThemeProvider theme={themeDark}>
-				<Box style={{ width: "50%", backgroundColor: "black" }}>
-					<Button variant='contained'>Clieck</Button>
-				</Box>
-			</ThemeProvider>
-			<ThemeProvider theme={themeLight}>
-				<Button variant='contained'>Clieck</Button>
-			</ThemeProvider>
+			<FormControlLabel
+				control={<Switch />}
+				label={theme}
+				onChange={handleThemeChange}
+			/>
+			{theme == "Light" ? (
+				<ThemeProvider theme={themeLight}>
+					<Box style={{ width: "50%", backgroundColor: "black" }}>
+						<Button variant='contained'>Clieck</Button>
+					</Box>
+				</ThemeProvider>
+			) : (
+				<ThemeProvider theme={themeDark}>
+					<Box style={{ width: "50%", backgroundColor: "black" }}>
+						<Button variant='contained'>Clieck</Button>
+					</Box>
+				</ThemeProvider>
+			)}
+
+
 		</div>
 	);
 }
